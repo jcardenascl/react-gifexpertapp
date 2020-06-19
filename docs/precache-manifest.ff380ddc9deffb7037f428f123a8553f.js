0fa402eaa498cb999d9e3502a2fc6fad80fdd514
jcardenascl <jl.danelcardenas@gmail.com>
2020-06-18 23:50:02 -0400

    docs agregado

---
+++ docs/precache-manifest.ff380ddc9deffb7037f428f123a8553f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8141735354558c0f28cd6fd55288ab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d07e243a9078fd4d839",
+    "url": "/static/css/main.f10ac17b.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d07e243a9078fd4d839",
+    "url": "/static/js/main.d6856652.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
